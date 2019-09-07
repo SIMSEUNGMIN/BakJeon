c6f168a46860700f3a7fffb0ce1dd5b5cb689fab
SIMSEUNGMIN <ssm4591@naver.com>
2019-09-07 15:31:29 +0900

    add the code that case if input is 1

--- 1929/src/Problem.java
+++ 1929/src/Problem.java
@@ -10,6 +10,11 @@ public class Problem {
 		int min = scanner.nextInt();
 		int max = scanner.nextInt();
 		
+		//1은 소수가 아니다
+		if(min == 1) {
+			min = 2;
+		}
+		
 		for(int i = min; i <= max; i++) {
 			
 			int count = (int)Math.sqrt(i);
