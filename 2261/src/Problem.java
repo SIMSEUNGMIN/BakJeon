b4e2e4263ad3835d12a812ba5b2238702c98658b
SIMSEUNGMIN <ssm4591@naver.com>
2019-09-29 01:01:10 +0900

    change LinkedList to ArrayList and modify all function

--- 2261/src/Problem.java
+++ 2261/src/Problem.java
@@ -1,16 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Problem {
-	
-	static int min = 0;
 
 	public static void main(String[] args) throws NumberFormatException, IOException{
 		
@@ -18,7 +16,7 @@ public class Problem {
 		
 		int input = Integer.parseInt(bf.readLine());
 		
-		List<Point> array = new LinkedList<>();
+		List<Point> array = new ArrayList<>();
 		
 		for(int i = 0; i < input; i++) {
 			StringTokenizer st = new StringTokenizer(bf.readLine());
@@ -29,81 +27,76 @@ public class Problem {
 		Collections.sort(array, new PointComparatorX());
 		
 		//정렬된 array로 Closest_pair구한 결과 출력
-		System.out.print((int)closestPair(array, 0, array.size()));
+		System.out.print(closestPair(array, 0, array.size()));
 	}
 
 	private static int closestPair(List<Point> array, int start, int end) {
 		
-		List<Point> tmpArray = new LinkedList<>();
+		List<Point> tmpArray = new ArrayList<>();
 		
-		if((end - start) <= 3) {
 			
-			switch(end-start) {
-				case 2:
-					//두 점 사이가 가장 최솟값
-					min = distance(array, 0, 1);
-					return min;
-				case 3:
-					int[] tmpDistance = new int[3];
-					
-					//점들 사이의 거리
-					//0번 째 값과 1번 째 값
-					tmpDistance[0] = distance(array, 0, 1);
-					//1번 째 값과 2번 째 값
-					tmpDistance[1] = distance(array, 1, 2);
-					//2번 째 값과 0번 째 값
-					tmpDistance[2] = distance(array, 0, 2);
-					
-					//거리를 오름차순으로 정렬
-					Arrays.sort(tmpDistance);
-					min = tmpDistance[0];
-					
-					return min;
-				default:
-					return min;
-			}
+		switch(end-start) {
+		case 2:
+			//두 점 사이가 가장 최솟값
+			return distance(array.get(start), array.get(start+1));
+		case 3:
+			int[] tmpDistance = new int[3];
+
+			//점들 사이의 거리
+			//0번 째 값과 1번 째 값
+			tmpDistance[0] = distance(array.get(start), array.get(start+1));
+			//1번 째 값과 2번 째 값
+			tmpDistance[1] = distance(array.get(start+1), array.get(start+2));
+			//2번 째 값과 0번 째 값
+			tmpDistance[2] = distance(array.get(start), array.get(start+2));
+
+			//거리를 오름차순으로 정렬
+			Arrays.sort(tmpDistance);
+			return tmpDistance[0];
 		}
+		
 		//점의 개수가 4개 이상일 때
-		else {
-			//배열을 반으로 쪼개서 왼쪽과 오른쪽으로 나눈 뒤
-			//왼쪽과 오른쪽에서 각각 나온 최솟값 중에서 더 작은 값을 반환함
-			int mid = (start + end) /2;
-			min = Math.min(closestPair(array, start, mid), closestPair(array, mid, end));
-			
-			//구하고 나서 최솟값보다 작은 점들을 배열에서 제외
-			for(int i = start; i < end; i++) {
-				if(((end/2 - Math.sqrt(min)) <= array.get(i).x) && (array.get(i).x <= (end/2 + Math.sqrt(min)))) {
-					tmpArray.add(array.get(i));
-				}
-			}
-			
-			//Y 오름차순 기준 sorting
-			Collections.sort(tmpArray, new PointComparatorY());
-			
-			//window 내부의 최단 거리를 구함
-			for(int one = 0; one < tmpArray.size()-1; one++) {
-				for(int two = one + 1; two < tmpArray.size(); two++) {
-					int tmp = tmpArray.get(one).y - tmpArray.get(two).y;
-					
-					if(tmp*tmp < min) {
-						int tmpD = distance(tmpArray, one, two);
-						if(tmpD < min) {
-							min = tmpD;
-						}
-					}
-					else {
-						break;
-					}
-				}
+		//배열을 반으로 쪼개서 왼쪽과 오른쪽으로 나눈 뒤
+		//왼쪽과 오른쪽에서 각각 나온 최솟값 중에서 더 작은 값을 반환함
+		int mid = (start + end) /2;
+		int min = Math.min(closestPair(array, start, mid), closestPair(array, mid, end));
+
+		//구하고 나서 최솟값보다 작은 점들을 배열에서 제외
+		for(int i = start; i < end; i++) {
+			int window = array.get(mid).x - array.get(i).x;
+			if((window * window) < min) {
+				tmpArray.add(array.get(i));
 			}
 		}
 		
+		//Y 오름차순 기준 sorting
+		Collections.sort(tmpArray, new PointComparatorY());
+
+		//window 내부의 최단 거리를 구함
+		for(int one = 0; one < tmpArray.size()-1; one++) {
+			for(int two = one + 1; two < tmpArray.size(); two++) {
+				int tmp = tmpArray.get(two).y - tmpArray.get(one).y;
+
+				if(tmp*tmp < min) {
+					int tmpD = distance(tmpArray.get(one), tmpArray.get(two));
+					if(tmpD < min) {
+						min = tmpD;
+					}
+				}
+				else {
+					break;
+				}
+			}
+		}
+
 		return min;
 	}
 	
-	private static int distance(List<Point> array, int left, int right) {
-		return (int) (Math.pow((array.get(left).x - array.get(right).x), 2) 
-				+ Math.pow((array.get(left).y - array.get(right).y), 2));
+	private static int distance(Point left, Point right) {
+		int XValue = left.x - right.x;
+		int YValue = left.y - right.y;
+		
+		return (XValue * XValue) + (YValue * YValue);
 	}
 }
 
@@ -115,7 +108,9 @@ class PointComparatorX implements Comparator<Point>{
 		if(left.x > right.x) {
 			return 1;
 		}
-		
+		else if(left.x == right.x){
+			return left.y - right.y;
+		}
 		return -1;
 	}
 }
@@ -125,10 +120,9 @@ class PointComparatorY implements Comparator<Point>{
 	@Override
 	public int compare(Point left, Point right) {
 		// Y에 대해서 오름 차순
-		if(left.y > right.y) {
+		if(left.y >= right.y) {
 			return 1;
 		}
-		
 		return -1;
 	}
 }
