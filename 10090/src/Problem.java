c7500b3ff841c81bd84189487a1e518c27d5ae17
SIMSEUNGMIN <ssm4591@naver.com>
2019-09-29 23:58:41 +0900

    change the String array to int array

--- 10090/src/Problem.java
+++ 10090/src/Problem.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class Problem {
 	
 	static int inversionCount = 0;
-	static String[] array;
+	static int[] array;
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		// TODO Auto-generated method stub
@@ -17,7 +17,7 @@ public class Problem {
 		
 		bf.readLine();
 
-		array = (bf.readLine()).split(" ");
+		array = Arrays.stream((bf.readLine()).split(" ")).mapToInt(Integer::parseInt).toArray();
 		
 		//Sort 실행
 		inversion(0, array.length);
@@ -38,46 +38,38 @@ public class Problem {
 	
 	private static void mergeInversion(int start, int mid, int end) {
 		
-		String[] sortedList = new String[array.length];
+		int[] sortedList = new int[array.length];
 		
 		int sortedListCount = 0;
 	
 		int indexFirst = start;
 		int indexSecond = mid;
 		
-		//남은 개수
-		int remindFirst = mid-start;
-		int remindSecond = end-mid;
-		
-		while(remindFirst != 0 && remindSecond != 0) {
+		while(indexFirst < mid && indexSecond < end) {
 			//앞 배열의 원소가 뒷 배열의 원소보다 클 경우 (inversion수와 뒷 배열의 원소가 sortList배열로 들어감)
-			if(Integer.parseInt(array[indexFirst]) > Integer.parseInt(array[indexSecond])) {
+			if(array[indexFirst] > array[indexSecond]) {
 				//자기 자신 + 뒤에 남은 개수 더해서 inversion에 추가
-				inversionCount += remindFirst;
+				inversionCount += mid-indexFirst;
 				sortedList[sortedListCount++] = array[indexSecond++];
-				remindSecond--;
 			}
 			//뒷 배열의 원소가 앞 배열의 원소보다 같거나 클 경우(앞 배열의 원소가 sortList배열로 들어감)
 			else {
 				//뒷 배열이 큰 건 당연하기 때문에 inversion 증가 X
 				sortedList[sortedListCount++] = array[indexFirst++];
-				remindFirst--;
 			}
 		}
 		
 		//둘 중 하나라도 0이라면 나머지는 배열 뒤로 옮김
-		if(remindSecond != 0) {
+		if(indexSecond < end) {
 			//뒷 배열이 남았을 경우 뒷 배열의 남은 원소가 0이 될 때까지 sortedList에 원소를 넣음
-			while(remindSecond != 0) {
+			while(indexSecond != end) {
 				sortedList[sortedListCount++] = array[indexSecond++];
-				remindSecond--;
 			}
 		}
 		//앞 배열이 남았을 경우
-		else if(remindFirst != 0){
-			while(remindFirst != 0) {
+		else if(indexFirst < mid){
+			while(indexFirst != mid) {
 				sortedList[sortedListCount++] = array[indexFirst++];
-				remindFirst--;
 			}
 		}
 		
